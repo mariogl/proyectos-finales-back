11bfa36ba86977d0ee8b830300dff3eace19f854
Mario González <mario@mariogl.com>
2022-03-06 17:28:37 +0100

    Database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("proyectos-api:server:initialize");
 const chalk = require("chalk");
+const connectDB = require("./database");
 const app = require("./server");
 const initializeServer = require("./server/initializeServer");
 
@@ -8,9 +9,17 @@ const port = +process.env.PORT || 4000;
 
 (async () => {
   try {
-    await initializeServer(app, port);
+    await connectDB(process.env.MONGODB_CONNECTION).catch((error) => {
+      debug(chalk.red(`Error al iniciar la base de datos: ${error.message}`));
+      throw new Error();
+    });
+    debug(chalk.blue("Base de datos iniciada"));
+    await initializeServer(app, port).catch((error) => {
+      debug(chalk.red(`Error al iniciar el servidor: ${error.message}`));
+      throw new Error();
+    });
     debug(chalk.yellow(`Servidor iniciado en http://localhost:${port}`));
   } catch (error) {
-    debug(chalk.red(`Error al iniciar el servidor: ${error.message}`));
+    process.exit(1);
   }
 })();
