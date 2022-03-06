7820aaae93dc7e53155f920d4fd84b93f68c1c78
Mario González <mario@mariogl.com>
2022-03-06 17:23:59 +0100

    Added morgan & helmet middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
 const { generalError, notFoundError } = require("./middlewares/errors");
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.use(notFoundError);
 app.use(generalError);
