f18a9fce8f9bfd1bb24a3dee8ccc18dd2f0b5064
Mario González <mario@mariogl.com>
2022-03-07 16:47:45 +0100

    Tested user login endpoint

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = (connectionString) =>
   new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
     mongoose.connect(connectionString, (error) => {
       if (error) {
         return reject(error);
