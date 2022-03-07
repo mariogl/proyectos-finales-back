e69726e4861c00a1dc7d290d46b11860d3b6c853
Mario González <mario@mariogl.com>
2022-03-07 20:15:21 +0100

    Activated virtuals and deleted _id & __v fields

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 const connectDB = (connectionString) =>
   new Promise((resolve, reject) => {
     mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        const newRet = { ...ret };
+        delete newRet.__v;
+        delete newRet._id;
+        return newRet;
+      },
+    });
     mongoose.connect(connectionString, (error) => {
       if (error) {
         return reject(error);
