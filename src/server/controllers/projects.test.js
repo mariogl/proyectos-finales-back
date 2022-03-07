55443badf24247ed535dfa02d3639bce213ea9bd
Mario González <mario@mariogl.com>
2022-03-07 20:59:08 +0100

    Tested getAllProjects controller

---
+++ src/server/controllers/projects.test.js
@@ -0,0 +1,22 @@
+const Project = require("../../database/models/Project");
+const { getAllProjects } = require("./projects");
+
+jest.mock("../../database/models/Project");
+
+describe("Given a getAllProjects controller", () => {
+  describe("When it receives a response object", () => {
+    test("Then it should invoke the response's method json with a list of projects", async () => {
+      const res = {
+        json: jest.fn(),
+      };
+      const expectedProjects = { projects: [] };
+
+      Project.find = jest.fn().mockReturnThis();
+      Project.sort = jest.fn().mockResolvedValue(expectedProjects.projects);
+
+      await getAllProjects(null, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedProjects);
+    });
+  });
+});
